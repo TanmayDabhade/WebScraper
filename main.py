d685a475bd04108ad5e7008377abbe5fcdc228de
TanmayDabhade <107470247+TanmayDabhade@users.noreply.github.com>
2024-05-15 01:46:00 +0530

    Update main.py
    Added Docstrings

--- main.py
+++ main.py
@@ -10,7 +10,26 @@ def index():
 
 @app.route('/scrape', methods=['POST'])
 def scrapePage():
-    ''''''
+    '''
+    Scrape a webpage and extract specified elements' attributes.
+
+    This function handles POST requests to the '/scrape' route. It accepts two form parameters:
+    - 'url': The URL of the webpage to scrape.
+    - 'element': The HTML element to extract from the webpage.
+
+    The function performs the following actions:
+    1. Retrieves the URL and element type from the form data.
+    2. Sends a GET request to the provided URL to fetch the webpage content.
+    3. Parses the webpage content using BeautifulSoup.
+    4. Finds all instances of the specified HTML element in the webpage.
+    5. Extracts and collects the attributes of these elements.
+    6. Renders an HTML template ('attributes.html') with the extracted attributes.
+
+    If no elements of the specified type are found, it returns a message indicating no elements were found.
+
+    Returns:
+        A rendered HTML template displaying the attributes of the specified elements, or a message if no elements are found.
+    '''
     link = request.form['url']
     element_to_extract = request.form['element']
     page = requests.get(link)
@@ -26,7 +45,30 @@ def scrapePage():
 
 @app.route('/choose', methods=['POST'])
 def getAttributes():
-    '''This function is the scraping script, which manages the'''
+    '''
+    Scrapes a web page for specific HTML elements and retrieves the values of a specified attribute.
+
+    This function is triggered by a POST request containing a URL, an HTML element to search for, and 
+    an attribute to retrieve from the elements. It sends a GET request to the specified URL, parses the 
+    HTML content, and finds all occurrences of the specified element. It then extracts the values of the 
+    specified attribute from each element and renders a template displaying these values.
+
+    Returns:
+        A rendered HTML template displaying the values of the specified attribute for the found elements.
+
+    Request form parameters:
+        url (str): The URL of the web page to scrape.
+        element (str): The HTML element to search for in the web page.
+        attribute (str): The attribute whose values need to be retrieved from the found elements.
+
+    Renders:
+        results.html: A template that displays the values of the specified attribute for the found elements.
+
+    Example:
+        Sending a POST request with 'url' set to 'http://example.com', 'element' set to 'a', and 
+        'attribute' set to 'href' will scrape 'http://example.com' for all <a> tags and display their 'href' attribute values.
+
+    '''
     attribute = request.form['attribute']
     link = request.form['url']
     elements_to_extract = request.form['element']
